ec0808713c6142802664f5aa0fed1d248e5db0d5
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-09-21 22:39:16 +0300

    Some Changes In Car Entity

--- src/main/java/com/gr1tEnt/dealership/models/Car.java
+++ src/main/java/com/gr1tEnt/dealership/models/Car.java
@@ -21,8 +21,6 @@ public class Car {
     private String model;
     private String color;
     private int mileage;
-
-    @Column(name = "image_name")
     private String imageFileName;
 
     @Column(name = "production_year")
