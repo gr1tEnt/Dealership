0f656b2c489aa8f3d1cc819bb64e027277915c4d
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-09-22 17:53:46 +0300

    Create CarDto

--- src/main/java/com/gr1tEnt/dealership/Controllers/CarsController.java
+++ src/main/java/com/gr1tEnt/dealership/Controllers/CarsController.java
@@ -3,11 +3,15 @@ package com.gr1tEnt.dealership.Controllers;
 import com.gr1tEnt.dealership.models.Car;
 import com.gr1tEnt.dealership.models.CarDto;
 import com.gr1tEnt.dealership.services.CarsRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,5 +35,19 @@ public class CarsController {
     public String showAddPage(Model model) {
         model.addAttribute("carDto", new CarDto());
         return "cars/AddCar";
+    }
+
+    @PostMapping("/add")
+    public String addCar(@Valid @ModelAttribute CarDto carDto,
+                         BindingResult result) {
+        if(carDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("carDto", "imageFile", "Image is required"));
+        }
+
+        if (result.hasErrors()) {
+            return "cars/AddCar";
+        }
+
+        return "redirect:/cars";
     }
 }
