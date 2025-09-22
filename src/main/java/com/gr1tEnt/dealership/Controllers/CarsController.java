6c086e2368263ec5c6fcbc45c73755c6e3717f81
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-09-22 17:38:46 +0300

    Create method showAddPage

--- src/main/java/com/gr1tEnt/dealership/Controllers/CarsController.java
+++ src/main/java/com/gr1tEnt/dealership/Controllers/CarsController.java
@@ -1,12 +1,14 @@
 package com.gr1tEnt.dealership.Controllers;
 
 import com.gr1tEnt.dealership.models.Car;
+import com.gr1tEnt.dealership.models.CarDto;
 import com.gr1tEnt.dealership.services.CarsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +25,11 @@ public class CarsController {
         List<Car> cars = carsRepository.findAll(Sort.by(Sort.Direction.ASC, "mileage"));
         model.addAttribute("cars", cars);
         return "cars/index";
+    }
+
+    @GetMapping("/add")
+    public String showAddPage(Model model) {
+        model.addAttribute("carDto", new CarDto());
+        return "cars/AddCar";
     }
 }
