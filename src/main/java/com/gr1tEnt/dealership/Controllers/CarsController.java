3bdeab69d83ee369bab1c02273c9ff23cfabe24e
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-09-22 16:21:05 +0300

    Sorting cars by mileage (why not)

--- src/main/java/com/gr1tEnt/dealership/Controllers/CarsController.java
+++ src/main/java/com/gr1tEnt/dealership/Controllers/CarsController.java
@@ -3,6 +3,7 @@ package com.gr1tEnt.dealership.Controllers;
 import com.gr1tEnt.dealership.models.Car;
 import com.gr1tEnt.dealership.services.CarsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +20,7 @@ public class CarsController {
 
     @GetMapping({"", "/"})
     public String showCars(Model model) {
-        List<Car> cars = carsRepository.findAll();
+        List<Car> cars = carsRepository.findAll(Sort.by(Sort.Direction.ASC, "mileage"));
         model.addAttribute("cars", cars);
         return "cars/index";
     }
