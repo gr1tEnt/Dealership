0b2e3446f19e5295f877779593ce31dc4fb51028
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-09-22 23:11:45 +0300

    Add saving cars to DB

--- src/main/java/com/gr1tEnt/dealership/Controllers/CarsController.java
+++ src/main/java/com/gr1tEnt/dealership/Controllers/CarsController.java
@@ -10,11 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.*;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -45,9 +47,40 @@ public class CarsController {
             return "cars/AddCar";
         }
 
-        if(carDto.getImageFile().isEmpty()) {
+        if (carDto.getImageFile().isEmpty()) {
             result.addError(new FieldError("carDto", "imageFile", "Image is required"));
         }
+
+        MultipartFile image = carDto.getImageFile();
+        Date createdAt = new Date();
+        String imageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
+
+        try {
+            String uploadDir = "public/images/";
+            Path uploadPath = Paths.get(uploadDir);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + imageFileName), StandardCopyOption.REPLACE_EXISTING);
+            }
+
+        } catch (IOException e) {
+            System.out.printf("IOException: %s\n", e.getMessage());
+            ;
+        }
+
+        Car car = new Car();
+        car.setModel(carDto.getModel());
+        car.setDescription(carDto.getDescription());
+        car.setColor(carDto.getColor());
+        car.setMileage(carDto.getMileage());
+        car.setPrice(carDto.getPrice());
+        car.setProductionYear(carDto.getProductionYear());
+        car.setImageFileName(imageFileName);
+        carsRepository.save(car);
 
         return "redirect:/cars";
     }
