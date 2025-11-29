8f8cd8a2593eea88f43a75c3e1e49cbd65afe5d1
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-11-29 22:42:33 +0200

    Create save() in LocalFileStorageService

--- src/main/java/com/gr1tEnt/dealership/services/LocalFileStorageService.java
+++ src/main/java/com/gr1tEnt/dealership/services/LocalFileStorageService.java
@@ -2,13 +2,18 @@ package com.gr1tEnt.dealership.services;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 @Service
-public class LocalFileStorageService {
+public class LocalFileStorageService implements FileStorageService {
 
     private final Path rootLocation;
 
@@ -23,5 +28,29 @@ public class LocalFileStorageService {
         } catch (Exception e) {
             throw new RuntimeException("Could not initialize storage", e);
         }
+    }
+
+    @Override
+    public String save(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new IllegalArgumentException("Failed to store empty file.");
+            }
+
+            String fileName = UUID.randomUUID().toString();
+
+            Path filePath = rootLocation.resolve(fileName)
+                    .normalize().toAbsolutePath();
+
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+
+            }
+            return fileName;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store file", e);
+        }
+
     }
 }
