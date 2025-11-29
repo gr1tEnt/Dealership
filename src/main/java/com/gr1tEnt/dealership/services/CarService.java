1ae7e126bdc5ad583ff8a7ca5a52a1077d9a9535
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-11-29 23:29:35 +0200

    Create CarService with updateCar()

---
+++ src/main/java/com/gr1tEnt/dealership/services/CarService.java
@@ -0,0 +1,40 @@
+package com.gr1tEnt.dealership.services;
+
+import com.gr1tEnt.dealership.models.Car;
+import com.gr1tEnt.dealership.models.CarDto;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor // Lombok for constructor injection
+public class CarService {
+    private final CarsRepository carsRepository;
+    private final FileStorageService fileStorageService;
+
+    @Transactional
+    public void updateCar(UUID id, CarDto carDto) {
+        Car car = carsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid car id: " + id));
+
+        car.setModel(carDto.getModel())
+                .setDescription(carDto.getDescription())
+                .setColor(carDto.getColor())
+                .setMileage(carDto.getMileage())
+                .setPrice(carDto.getPrice())
+                .setProductionYear(carDto.getProductionYear());
+
+        if (carDto.getImageFile() != null && !carDto.getImageFile().isEmpty()) {
+            if (car.getImageFileName() != null) {
+                fileStorageService.delete(car.getImageFileName());
+            }
+
+            String newFileName = fileStorageService.save(carDto.getImageFile());
+
+            car.setImageFileName(newFileName);
+        }
+        carsRepository.save(car);
+    }
+}
