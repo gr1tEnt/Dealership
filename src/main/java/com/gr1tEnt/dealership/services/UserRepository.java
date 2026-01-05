d548685a9069ba70435dfdfdd5934a94f86bb838
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2026-01-05 13:57:18 +0200

    Create UserRepository

---
+++ src/main/java/com/gr1tEnt/dealership/services/UserRepository.java
@@ -0,0 +1,14 @@
+package com.gr1tEnt.dealership.services;
+
+import com.gr1tEnt.dealership.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
