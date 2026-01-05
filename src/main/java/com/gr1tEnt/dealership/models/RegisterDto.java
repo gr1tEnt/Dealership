396e9b70470b99107a71dfffd76d9527dbd07922
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2026-01-05 13:53:10 +0200

    Create RegisterDto

---
+++ src/main/java/com/gr1tEnt/dealership/models/RegisterDto.java
@@ -0,0 +1,34 @@
+package com.gr1tEnt.dealership.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class RegisterDto {
+
+    @NotEmpty(message = "Username is required")
+    @Size(min = 3, max = 20)
+    String username;
+
+    @Email
+    @NotEmpty(message = "Email is required")
+    String email;
+
+    @Size(min = 8)
+    @NotEmpty(message = "Email is required")
+    String password;
+
+    @NotEmpty(message = "Password confirmation is required")
+    String confirmPassword;
+
+    @NotEmpty(message = "First name is required")
+    String firstName;
+
+    @NotEmpty(message = "Last name is required")
+    String lastName;
+
+}
